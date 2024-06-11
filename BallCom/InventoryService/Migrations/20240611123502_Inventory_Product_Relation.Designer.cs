e50ad9a0479a13e7331201ce5994a2568c268ccd
Thimo Luijsterburg <Thimo.luijsterburg@rietveld.nl>
2024-06-11 14:35:52 +0200

    Relationship between product and inventory added

--- BallCom/InventoryService/Migrations/20240611123502_Inventory_Product_Relation.Designer.cs
+++ BallCom/InventoryService/Migrations/20240611123502_Inventory_Product_Relation.Designer.cs
@@ -3,6 +3,7 @@ using InventoryService.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryService.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240611123502_Inventory_Product_Relation")]
+    partial class Inventory_Product_Relation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BallCom/InventoryService/Migrations/20240611123502_Inventory_Product_Relation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inventory_Product_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Inventories_ProductId",
+                table: "Inventories",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inventories_Products_ProductId",
+                table: "Inventories",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inventories_Products_ProductId",
+                table: "Inventories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Inventories_ProductId",
+                table: "Inventories");
+        }
+    }
+}
