787a1538b7519170fe3aaced6a4569b5dc50433d
Sybrand B. <snjw.bos@student.avans.nl>
2024-06-13 16:37:00 +0200

    Product controller + repo

---
+++ BallCom/InventoryService/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using InventoryService.Domain;
+using InventoryService.Endpoints;
+using InventoryService.EventHandlers.Interfaces;
+using InventoryService.Events;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Repository.Interface;
+
+namespace InventoryService.Controllers
+{
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IReadRepository<Product> _productRepository;
+
+        public ProductController(ILogger<ProductController> logger, IReadRepository<Product> productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+
+        [HttpGet]
+        public async Task<Product> Get(Guid id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            // Save the inventory to the inventory table
+            _productRepository.CreateAsync(product);
+        }
+
+        [HttpDelete]
+        public void Delete([FromBody] Product product)
+        {
+            // Remove the inventory from the inventory table
+            _productRepository.RemoveAsync(product);
+        }
+    }
+}
---
+++ BallCom/InventoryService/EventHandlers/ProductEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using InventoryService.Events;
+
+namespace InventoryService.EventHandlers
+{
+    public class ProductEventHandler
+    {
+        public void Handle(int productId)
+        {
+            
+        }
+    }
+}
