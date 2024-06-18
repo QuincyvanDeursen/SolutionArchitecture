8b54f11b0816da40ada5deaf05ebf6db2fd635ae
Thimo Luijsterburg <Thimo.luijsterburg@rietveld.nl>
2024-06-18 11:41:41 +0200

    service function added

--- BallCom/InventoryService/Services/RabbitMQ/InventoryMessageListenerService.cs
+++ BallCom/InventoryService/Services/RabbitMQ/InventoryMessageListenerService.cs
@@ -1,12 +1,16 @@
 
 
+using InventoryService.Domain;
+using InventoryService.Dto;
+using InventoryService.Services.Interfaces;
 using Shared.MessageBroker;
 using Shared.MessageBroker.Consumer.Interfaces;
 
 namespace InventoryService.Services.RabbitMQ;
 
-public class InventoryMessageListenerService(IMessageConsumer messageConsumer) : IHostedService
+public class InventoryMessageListenerService(IMessageConsumer messageConsumer, IInventoryService _inventoryService) : IHostedService
 {
+    private readonly IInventoryService _inventoryService = _inventoryService;
     private readonly IMessageConsumer _messageConsumer = messageConsumer;
 
     public async Task StartAsync(CancellationToken cancellationToken)
@@ -20,13 +24,17 @@ public class InventoryMessageListenerService(IMessageConsumer messageConsumer) :
 
     public async Task OnMessageReceived(MessageEventData<object> data)
     {
+        var product = (Product)data.Data;
+
         switch (data.Topic)
         {
             case "inventory.create":
                 Console.WriteLine();
+                await CreateInventory(product);
                 break;
             case "inventory.update":
                 Console.WriteLine();
+                await UpdateInventory(product.Id, product);
                 break;
             default:
                 Console.WriteLine();
@@ -41,5 +49,34 @@ public class InventoryMessageListenerService(IMessageConsumer messageConsumer) :
     {
         // TODO: Implement the stop logic for the connection
         return Task.CompletedTask;
+    }
+
+    private async Task CreateInventory(Product product)
+    {
+        var prodDto = new ProductCreateDto()
+        {
+            Name = product.Name,
+            Description = product.Description,
+            Price = product.Price,
+            Quantity = product.Quantity
+        };
+
+        await _inventoryService.AddProduct(prodDto);
+    }
+
+    private async Task UpdateInventory(Guid id, Product product)
+    {
+        var oldProduct = await _inventoryService.GetProduct(id);
+
+        var quantity = oldProduct.Quantity + product.Quantity;
+
+        if (quantity < 0) return;
+
+        var prodDto = new ProductUpdateDto
+        {
+            Quantity = quantity
+        };
+
+        await _inventoryService.UpdateProduct(id, prodDto);
     }
 }
