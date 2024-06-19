80825dcebc90f1d4feccfdadbad1e389f8f09b5c
Sybrand B. <snjw.bos@student.avans.nl>
2024-06-19 16:59:57 +0200

    RabbitMQ working, dependency injection not

--- BallCom/PaymentService/Services/RabbitMQ/OrderMessageListenerService.cs
+++ BallCom/PaymentService/Services/RabbitMQ/OrderMessageListenerService.cs
@@ -2,41 +2,53 @@
 using PaymentService.Services.Interfaces;
 using Shared.MessageBroker;
 using Shared.MessageBroker.Consumer.Interfaces;
+using System.Text.Json;
 
 namespace PaymentService.Services.RabbitMQ
 {
-    public class OrderMessageListenerService(IMessageConsumer messageConsumer, IOrderEventHandlerService orderEventHandlerService)
+    public class OrderMessageListenerService(IMessageConsumer messageConsumer, IOrderEventHandlerService orderEventHandlerService, IServiceProvider serviceProvider)
     {
         private readonly IOrderEventHandlerService _orderEventHandlerService = orderEventHandlerService;
+        private readonly IServiceProvider _serviceProvider = serviceProvider;
         private readonly IMessageConsumer _messageConsumer = messageConsumer;
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            await messageConsumer.ConsumeAsync<MessageEventData<object>>(OnMessageReceived, new[]
+            await messageConsumer.ConsumeAsync(OnMessageReceived, new[]
             {
-                "order.create",
+            "order.create",
                 "order.update",
                 "customer.create",
                 "customer.update"
-            });
+        });
         }
 
-        public async Task OnMessageReceived(MessageEventData<object> data)
+        public async Task OnMessageReceived(MessageEventData data)
         {
+            // Create a new scope for the event handler service (scoped service)
+            using var scope = _serviceProvider.CreateScope();
+            var eventHandlerService = scope.ServiceProvider.GetService<IOrderEventHandlerService>() ??
+                                      throw new ArgumentNullException(nameof(IOrderEventHandlerService));
+
             switch (data.Topic)
             {
                 case "inventory.create":
-                    var createOrder = (Order)data.Data;
+                    var createOrder = JsonSerializer.Deserialize<Order>(data.DataJson);
                     await _orderEventHandlerService.AddOrder(createOrder);
                     break;
                 case "inventory.update":
-                    var updateOrder = (Order)data.Data;
+                    var updateOrder = JsonSerializer.Deserialize<Order>(data.DataJson);
                     await _orderEventHandlerService.UpdateOrder(updateOrder);
                     break;
                 default:
-                    Console.WriteLine();
-                    throw new ArgumentException(data.Topic + " is not a listed topic.");
+                    throw new ArgumentException(data.Topic + " is not a subscribed topic.");
             }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Implement the stop logic for the connection
+            return Task.CompletedTask;
         }
     }
 }
