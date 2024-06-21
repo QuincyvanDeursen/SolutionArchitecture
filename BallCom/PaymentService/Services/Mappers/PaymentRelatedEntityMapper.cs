3af00d1290565323c38f1b1efa648a70a34eabfa
Tristan Goossens <tt.goossens@student.avans.nl>
2024-06-21 15:18:08 +0200

    added a mapper to store only required info for related entities

---
+++ BallCom/PaymentService/Services/Mappers/PaymentRelatedEntityMapper.cs
@@ -0,0 +1,29 @@
+using PaymentService.Domain;
+using Shared.Models;
+
+namespace PaymentService.Services.Mapper;
+
+public static class PaymentRelatedEntityMapper 
+{
+    public static PaymentOrder MapOrderToPaymentOrder(Order order)
+    {
+        return new PaymentOrder
+        {
+            Id = order.Id,
+            TotalPrice = order.TotalPrice,
+            Address = order.Address,
+            Status = order.Status,
+            CustomerId = order.CustomerId
+        };
+    } 
+    
+    public static PaymentCustomer MapCustomerToPaymentCustomer(Customer customer)
+    {
+        return new PaymentCustomer
+        {
+            Id = customer.Id,
+            Name = $"{customer.FirstName} {customer.LastName}",
+            Address = customer.Address
+        };
+    }
+}
