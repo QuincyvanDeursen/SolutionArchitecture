e655b28efa6151374ec8a112f45a083749aa7e89
Tristan Goossens <tt.goossens@student.avans.nl>
2024-06-21 15:16:13 +0200

    updated listener service to accept only interested topics

--- BallCom/PaymentService/Services/RabbitMQ/PaymentMessageListenerService.cs
+++ BallCom/PaymentService/Services/RabbitMQ/PaymentMessageListenerService.cs
@@ -1,24 +1,47 @@
+using System.Text.Json;
+using PaymentService.Services.Interfaces;
+using PaymentService.Services.Mapper;
+using PaymentService.Services.RabbitMQ.EventHandlers.Interfaces;
 using Shared.MessageBroker;
 using Shared.MessageBroker.Consumer.Interfaces;
+using Shared.Models;
 
 namespace PaymentService.Services.RabbitMQ;
 
-public class PaymentMessageListenerService(IMessageConsumer messageConsumer) : IHostedService
+public class PaymentMessageListenerService(IMessageConsumer messageConsumer, IServiceProvider serviceProvider) : IHostedService
 {
-    private readonly IMessageConsumer _messageConsumer = messageConsumer;
-
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         await messageConsumer.ConsumeAsync(OnMessageReceived, new []
         {
-            "payment.*"
+            "order.create",
+            "customer.create",
+            "customer.update",
         });
     }
 
     public async Task OnMessageReceived(MessageEventData data)
     {
-        // TODO: Based on the topic, do the appropriate action
-        Console.WriteLine($"[{data.Timestamp}] Received message: ({data.Topic} - {data.Id})");
+        // Create a new scope for the event handler service (scoped service)
+        using var scope = serviceProvider.CreateScope();
+        var orderEventHandlerService = scope.ServiceProvider.GetRequiredService<IOrderEventHandlerService>();  
+        var customerEventHandlerService = scope.ServiceProvider.GetRequiredService<ICustomerEventHandlerService>();
+        
+        switch (data.Topic)
+        {
+            case "customer.create":
+                var createdCustomer = JsonSerializer.Deserialize<Customer>(data.DataJson);
+                await customerEventHandlerService.ProcessCustomerCreateEvent(PaymentRelatedEntityMapper.MapCustomerToPaymentCustomer(createdCustomer));
+                break;
+            case "customer.update":
+                var updatedCustomer = JsonSerializer.Deserialize<Customer>(data.DataJson);
+                await customerEventHandlerService.ProcessCustomerUpdateEvent(PaymentRelatedEntityMapper.MapCustomerToPaymentCustomer(updatedCustomer));
+                break;
+            case "order.create":
+                var createdOrder = JsonSerializer.Deserialize<Order>(data.DataJson);
+                await orderEventHandlerService.ProcessOrderCreateEvent(PaymentRelatedEntityMapper.MapOrderToPaymentOrder(createdOrder));
+                break;
+        }
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
